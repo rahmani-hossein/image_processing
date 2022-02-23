0533a42dd39af00b46224af4e347130c859c4e45
hossein <61795938+rahmani-hossein@users.noreply.github.com>
2022-02-23 22:18:00 +0330

    Delete q5.py

--- q5.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2 as cv
-import time
-
-st=time.time()
-pink=cv.imread("C:/Users/hossein rahmani/Desktop/Pink.jpg")
-res=cv.boxFilter(pink,-1,(3,3),cv.BORDER_REPLICATE)
-print("----%.2f----"%(time.time()-st))
-cv.imwrite('C:/Users/hossein rahmani/PycharmProjects/begin/ex1/res3.jpg',res)
